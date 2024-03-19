b27403fdae1f21c7a0caa24cd1357c0f752c2ae8
sailalith <lalithvoop@hotmail.com>
2024-03-19 23:58:36 +0530

    added status check for mysql main.yml

--- main.sh
+++ main.sh
@@ -2,3 +2,6 @@ env = $0
 component = $1
 
 ansible-playbook -i $component-$env.vsldo.online, -e ansible_username=ec2-user -e ansible_password=DevOps321 -e env=$env -e role=$component main.yml
+
+
+# ansible-playbook -i mysql-dev.vsldo.online, -e ansible_username=ec2-user -e ansible_password=DevOps321 -e env=dev -e role=frontend main.yml
