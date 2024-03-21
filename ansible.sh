bd32a2aedaa57389f694b75c7fad36ec53eeba0b
lalithvoop <lalithvoop@hotmail.com>
2024-03-21 19:32:31 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 
-ansible-playbook -i $component-dev.vsldo.online, -e ansible_user=ec2-user -e ansible_password=DevOps321  -e role_name=$component expense.yml
+ansible-playbook -i $component-dev.vsldo.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
